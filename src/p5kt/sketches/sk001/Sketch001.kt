a5ccbe5ddb6073e533ef772423e3a78f5cd1ddc5
Fish <jonathan.fisher@nbrown.co.uk>
2019-10-11 21:53:18 +0100

    ...

--- src/p5kt/sketches/sk001/Sketch001.kt
+++ src/p5kt/sketches/sk001/Sketch001.kt
@@ -3,12 +3,11 @@ package p5kt.sketches.sk001
 import p5kt.KApplet
 import p5kt.colorLerp
 import p5kt.gridutils.BasicCell
-import processing.core.PConstants
 
 class Sketch001: KApplet() {
 
     override fun settings() {
-        size(600, 600, PConstants.P3D)
+        size(600, 600)
         super.settings()
     }
 
