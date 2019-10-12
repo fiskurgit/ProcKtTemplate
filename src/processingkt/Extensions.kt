5f7e1bfd1731592e5e4800a3d467d96275de6560
Fish <jonathan.fisher@nbrown.co.uk>
2019-10-12 13:44:31 +0100

    handle screenshots when not looping

--- src/processingkt/Extensions.kt
+++ src/processingkt/Extensions.kt
@@ -3,23 +3,10 @@ package processingkt
 import processing.core.PApplet
 import processing.core.PConstants
 import java.awt.Color
-import java.io.File
-import javax.swing.JFileChooser
 
 /*
     Only use extension functions for convenience overloads of existing Processing API
  */
-
-private var saveScreenshotChooser: JFileChooser? = null
-
-fun KApplet.screenshot(){
-    if(saveScreenshotChooser == null) saveScreenshotChooser = JFileChooser()
-    val filename = "processingkt_${System.currentTimeMillis()}.png"
-    saveScreenshotChooser?.selectedFile = File(filename)
-    val returnVal = saveScreenshotChooser?.showSaveDialog(this.frame)
-    if (returnVal == JFileChooser.APPROVE_OPTION) saveFrame(saveScreenshotChooser?.selectedFile?.path)
-}
-
 fun KApplet.color(color: String): Int{
     return Color.decode(color).rgb
 }
--- src/processingkt/KApplet.kt
+++ src/processingkt/KApplet.kt
@@ -5,15 +5,21 @@ import processing.core.PApplet
 import processing.event.KeyEvent
 import processing.opengl.PJOGL
 import java.awt.Color
+import java.io.File
+import java.nio.file.Files
+import java.nio.file.Paths
+import javax.swing.JFileChooser
 
 open class KApplet: PApplet() {
 
     val grid = Grid()
     private var commandKeyPressed = false
+    private var saveScreenshotChooser: JFileChooser? = null
 
     companion object {
         val BLACK = Color.BLACK.rgb
         val WHITE = Color.WHITE.rgb
+        const val NO_LOOP_TEMP_SCREENSHOT = "noloop_temp.png"
     }
 
     fun run() {
@@ -39,5 +45,32 @@ open class KApplet: PApplet() {
     override fun keyReleased() {
         commandKeyPressed = false
         super.keyReleased()
+    }
+
+    open fun screenshot(){
+        if(saveScreenshotChooser == null) saveScreenshotChooser = JFileChooser()
+        val filename = "processingkt_${System.currentTimeMillis()}.png"
+        saveScreenshotChooser?.selectedFile = File(filename)
+        val returnVal = saveScreenshotChooser?.showSaveDialog(this.frame)
+        if (returnVal == JFileChooser.APPROVE_OPTION){
+            if(isLooping){
+                saveFrame(saveScreenshotChooser?.selectedFile?.path)
+            }else{
+                val temp = File(NO_LOOP_TEMP_SCREENSHOT)
+                if(temp.exists()){
+                    Files.copy(Paths.get(temp.path), Paths.get(saveScreenshotChooser?.selectedFile!!.path))
+                }
+            }
+        }
+    }
+
+    override fun noLoop() {
+        saveFrame(NO_LOOP_TEMP_SCREENSHOT)
+        super.noLoop()
+    }
+
+    override fun loop() {
+        File(NO_LOOP_TEMP_SCREENSHOT).delete()
+        super.loop()
     }
 }
