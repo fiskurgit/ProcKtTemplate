89cb560ca4d9a24b09a64719eca50540091bb3da
Fish <jonathan.fisher@nbrown.co.uk>
2019-10-28 08:27:39 +0000

    update gravity sketch

--- src/prockt/sketches/sk012/Sketch012.kt
+++ src/prockt/sketches/sk012/Sketch012.kt
@@ -1,7 +1,10 @@
 package prockt.sketches.sk012
 
+import processing.core.PGraphics
+import processing.core.PImage
 import prockt.KApplet
 import processing.core.PVector
+import prockt.OneBitFilter
 
 /*
 
@@ -10,19 +13,27 @@ import processing.core.PVector
  */
 class Sketch012: KApplet() {
 
-    var bodies = mutableListOf<Body>()
-    var maxMass = 5f
-    var fmax = 0.00001f
-    var dist: Float = 0.toFloat()
-    var f = 0f
-    var maxVelocity = 0.01f
+    companion object {
+        const val SKETCH_DIAM = 600
+        const val MIN_MASS = 3f
+        const val MAX_MASS = 5f
+        const val MAX_VELOCITY = 0.01f
+        const val MAX_FORCE = 0.000001f
+        val FILTER = OneBitFilter.get("Atkinson").threshold(255)
+    }
 
+    var bodies = mutableListOf<Body>()
+    private var sourceImage: PGraphics? = null
+    private var filteredImage: PImage? = null
 
     override fun settings() {
         size(600, 600, P3D)
     }
 
     override fun setup() {
+        sourceImage = createGraphics(SKETCH_DIAM, SKETCH_DIAM, P3D)
+        filteredImage = PImage(SKETCH_DIAM, SKETCH_DIAM)
+        bodies.clear()
         repeat(100){
             bodies.add(Body())
         }
@@ -30,41 +41,62 @@ class Sketch012: KApplet() {
         fill(WHITE)
     }
 
+    override fun mouseClicked() {
+        setup()
+    }
+
 
     override fun draw() {
         background(BLACK)
-        lights()
+
+        updateSource()
+
+        FILTER.process(sourceImage!!, filteredImage!!)
+
+        when {
+            mousePressed -> image(sourceImage!!)
+            else -> image(filteredImage!!)
+        }
+    }
+
+    fun updateSource(){
+        sourceImage?.beginDraw()
+        sourceImage?.background(BLACK)
+        sourceImage?.noStroke()
+        sourceImage?.lights()
 
         bodies.forEach { body ->
             body.draw()
         }
+
+        sourceImage!!.endDraw()
     }
 
     inner class Body{
 
         var position= PVector(random(width.toFloat()), random(height.toFloat()))
-        val velocity = PVector(random(-maxVelocity, maxVelocity), random(-maxVelocity, maxVelocity))
-        var neighbour= PVector(0f, 0f)
-        var mass = ceil(random(maxMass))
+        val velocity = PVector(random(-MAX_VELOCITY, MAX_VELOCITY), random(-MAX_VELOCITY, MAX_VELOCITY))
+        var neighbour = PVector(0f, 0f)
+        var mass = ceil(random(MIN_MASS, MAX_MASS))
 
         fun draw(){
             bodies.forEach { otherBody ->
                 neighbour.set(otherBody.position)
-                dist=neighbour.dist(position)
-                f=(mass*otherBody.mass)/(sq(dist))
-                if(f>fmax) {
-                    f=fmax
+                var dist = neighbour.dist(position)
+                var force = (mass * otherBody.mass)/(sq(dist))
+                if(force > MAX_FORCE) {
+                    force = MAX_FORCE
                 }
                 neighbour.sub(position)
                 neighbour.normalize()
-                neighbour.mult(f/mass)
+                neighbour.mult(force / mass)
                 velocity.add(neighbour)
                 position.add(velocity)
             }
-            pushMatrix()
-            translate(position.x, position.y)
-            sphere(5f * mass)
-            popMatrix()
+            sourceImage!!.pushMatrix()
+            sourceImage!!.translate(position.x, position.y)
+            sourceImage!!.sphere(10f * mass)
+            sourceImage!!.popMatrix()
         }
     }
 }
