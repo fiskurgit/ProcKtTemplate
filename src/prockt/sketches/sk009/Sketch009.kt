03a8c6deeee6a133aee9271d61dfab7627e92e73
Fish <jonathan.fisher@nbrown.co.uk>
2019-10-28 11:36:42 +0000

    oops

--- src/prockt/sketches/sk009/Sketch009.kt
+++ src/prockt/sketches/sk009/Sketch009.kt
@@ -13,12 +13,12 @@ class Sketch009: KApplet() {
         repeat(50000){
             pushMatrix()
             translate(width/4, height/2)
-            point(randomCircleCoord(width/4))
+            point(randomCircleCoordWeighted(width/4))
             popMatrix()
 
             pushMatrix()
             translate((width/4) * 3, height/2)
-            point(randomCircleCoordC(width/4))
+            point(randomCircleCoord(width/4))
             popMatrix()
         }
 
