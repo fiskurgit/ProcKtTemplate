84f001692a9c8ec1a95d2a0613054a53cd3505a3
Fish <jonathan.fisher@nbrown.co.uk>
2019-11-23 20:53:58 +0000

    kvector attraction, nice.

--- src/prockt/sketches/Sketch029.kt
+++ src/prockt/sketches/Sketch029.kt
@@ -8,20 +8,22 @@ import prockt.api.KVector
     Attracted to nearest neighbour
 
  */
-
 class Sketch029: KApplet() {
 
     private val motes = mutableListOf<Mote>()
+
+    private val count = 200
 
     override fun settings() {
         size(800, 600)
     }
 
     override fun setup() {
-        repeat(60){
+        repeat(count){
             motes.add(Mote())
         }
         noStroke()
+        noCursor()
     }
 
     override fun draw() {
@@ -30,13 +32,13 @@ class Sketch029: KApplet() {
             mote.draw()
         }
 
-        fill(BLACK, 40)
+        fill(BLACK, 30)
         rect(0, 0, width, height)
     }
 
     override fun mousePressed() {
         motes.clear()
-        repeat(60){
+        repeat(count){
             motes.add(Mote())
         }
     }
@@ -45,11 +47,12 @@ class Sketch029: KApplet() {
         private var location = KVector(random(width), random(height))
         private var velocity = KVector(0f, 0f)
         private var acceleration: KVector? = null
-        private var maxSpeed = 5f
+        private var maxSpeed = 3f
+        var closestDistance = Float.MAX_VALUE
 
         fun update(): Mote{
             var closestMote: Mote? = null
-            var closestDistance = Float.MAX_VALUE
+            closestDistance = Float.MAX_VALUE
             motes.forEach {mote ->
                 val distance = location.distance(mote.location)
                 if(distance > 1 && distance < closestDistance){
@@ -59,7 +62,6 @@ class Sketch029: KApplet() {
             }
 
             var directionToMote = closestMote!!.location - location
-
             directionToMote.normalize()
             directionToMote *= 0.2f
 
@@ -75,7 +77,8 @@ class Sketch029: KApplet() {
         }
 
         fun draw() {
-            fill(MOLNAR)
+            val color = colorLerp("#9d2f4d", "#4973a1" , closestDistance, 20)
+            fill(color)
             ellipse(location.x, location.y, 4, 4)
         }
 
